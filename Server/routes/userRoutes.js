6e856751680e5d71ca801724887689aa348afb85
UsmanDrigrocha <UsmanDrigrocha@Gmail.com>
2023-10-23 10:58:28 +0500

    --updated comment

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -14,13 +14,13 @@ const {
     verifyOTP
 } = require('../controllers/otpController')
 
-//Public Routes
+//Auth  Routes
 route.post('/register', userRegister);
 route.post('/login', userLogin);
 route.post('/generate-otp', generateOTP);
-route.post('/verify-otp', verifyOTP) 
+route.post('/verify-otp', verifyOTP)
 route.post('/change-password', userChangePassword); // Change Old Password ✅
 route.post('/reset-password', userResetPassword); // Send link to Email
-route.get('/reset-password/:id/:token',verifyUserResetPassword)
+route.get('/reset-password/:id/:token', verifyUserResetPassword)
 
 module.exports = route;
