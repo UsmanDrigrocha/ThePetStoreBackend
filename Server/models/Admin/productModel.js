8d5a8ca198bf53309eef97b277cd9675689604fb
UsmanDrigrocha <UsmanDrigrocha@Gmail.com>
2023-11-06 17:26:17 +0500

    -- updated

--- Server/models/Admin/productModel.js
+++ Server/models/Admin/productModel.js
@@ -1,11 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const categorySchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     createdAt: { type: Date, default: Date.now },
-// });
-// module.exports = mongoose.model('productcategories', categorySchema);
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
